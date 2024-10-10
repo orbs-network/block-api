27463917105993eb20c3416d2c3e5d19ada7b40c
uvorbs <yuval@orbs.com>
2024-10-10 11:10:42 +0300

    fix deprecated express

--- src/index.js
+++ src/index.js
@@ -9,24 +9,24 @@ const port = process.env.PORT || 3000;
 let error = false;
 
 app.get('/api/health', async (req, res) => {
-  res.send(error ? "error" : "OK", error ? 500 : 200)
+  res.status(error ? 500 : 200).send(error ? "error" : "OK");
 })
 
 app.get('/api/blocktime', async (req, res) => {
   const block = req.query.block
   if (!block) {
     error = true;
-    return res.send("Missing 'block' query parameter", 400);
+    return res.status(400).send("Missing 'block' query parameter");
   }
 
   const blocktime = await getBlockTime(block)
   if (blocktime.error) {
     error = true;
-    return blocktime.send(blocktime.error, blocktime.status);
+    return res.status(blocktime.status).send(blocktime.error);
   }
 
   error = false;
-  res.send(blocktime, 200)
+  res.status(200).send(blocktime);
 })
 
 app.listen(port, () => {
