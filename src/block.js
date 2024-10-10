385f222e3eb89d4aab091c7947fb05b249f13d0c
uvorbs <yuval@orbs.com>
2024-10-10 08:41:47 +0300

    latest

--- src/block.js
+++ src/block.js
@@ -1,9 +1,14 @@
 const axios = require('axios');
+const RPC_URL = process.env.RPC_URL;
 
 async function getBlockTime(blocknum) {
 
-  // Retrieve the Infura Project ID from secr
-  const url = process.env.RPC_URL || `https://mainnet.infura.io/v3/572a08067371446795af6725be59bb46`;
+  // Retrieve the RPC Project ID from secr
+  if (!RPC_URL) {
+    const msg = "Error RPC URL is not set"
+    console.erro(msg, res);
+    return { error: msg, status: 500 };
+  }
 
   let hexBlock;
   if (blocknum.startsWith("0x")) {
@@ -16,40 +21,28 @@ async function getBlockTime(blocknum) {
     }
     hexBlock = "0x" + blockInt.toString(16);
   }
-  // Remove the host from the URL, use only the path
-  //const infuraUrl = `/v3/${INFURA_PROJECT_ID}`;
-  //const infuraUrl = ;
-  //const infuraUrl = `/v3/${INFURA_PROJECT_ID}`;
 
-
-  // Make the POST request to Infura
+  // Make the POST request to RPC
   let res;
   try {
-    // res = await fetch(url, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: `{ "jsonrpc": "2.0", "method": "eth_getBlockByNumber", "params": ["${blocknum}", false], "id": 1 }`,
-    //   //body: requestBody,
-    // });
+
     res = await axios({
       method: 'post',
-      url: url,
+      url: RPC_URL,
       data: { "jsonrpc": "2.0", "method": "eth_getBlockByNumber", "params": [hexBlock, false], "id": 1 }
     });
   } catch (error) {
-    console.error("Error contacting Infura", error);
+    console.error("Error contacting RPC", error);
     return { error: error.message, status: 502 };
   }
 
   if (res.status !== 200) {
-    console.erro("Error in Infura response", res);
+    console.erro("Error in RPC response", res);
     return { error: res.statusText, status: res.status };
   }
 
   if (res.data.error) {
-    console.erro("Error in Infura data", res.data.error);
+    console.erro("Error in RPC data", res.data.error);
     return { error: res.data.error, status: 500 };
   }
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const express = require('express')
 const getBlockTime = require('./block')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
+
 
 let error = false;
 
